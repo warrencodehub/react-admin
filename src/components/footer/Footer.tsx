c92eabd38ca5b9c4777cad40920aa842235e8a33
Miraflor <warren.miraflor@hartmannph.com>
2024-07-18 16:54:34 +0800

    first commit

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,12 @@
-import "./footer.scss"
+import "./footer.scss";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <span>lamadmin</span>
-      <span>© Lama Dev Admin Dashboard</span>
+      <span>reactadmin</span>
+      <span>© React Dev Admin Dashboard</span>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
