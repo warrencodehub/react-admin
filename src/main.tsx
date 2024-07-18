5c125f894197ad748f274fb9ab166b72aba22431
Miraflor <warren.miraflor@hartmannph.com>
2024-07-18 17:16:32 +0800

    commit fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,9 @@
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
